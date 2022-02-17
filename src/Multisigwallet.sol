a6a63304c6ba6b8391c9ebc59cf81c01417eac71
Tobias <thethirdpremise@gmail.com>
2022-02-17 13:45:42 -0500

    contract update-admin array

--- src/Multisigwallet.sol
+++ src/Multisigwallet.sol
@@ -1,31 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity >=0.8.4 <0.9.0;
 
 /**
- * @dev This is a multi-signature wallet that separates signers from admins.
+ * @notice This is a multi-signature wallet that separates Signers from Admins.
+ * @dev Admins can add other Admins and Signers and Execute Transactions. Signers can only Submit and Sign Transactions.
  */
-contract Multisigwallet {
+contract MultiSigWallet {
     uint8 signaturesRequired;
 
     address[] signers;
+
+    address[] admins;
 
     struct Transaction {
         address recipient;
         uint256 valueDue;
         bytes data;
-        uint8 numSigners;
+        uint8 signaturesCollected;
         bool completed;
     }
     //an array of structs
     Transaction[] public transactionsArray;
 
-    //grant the admin signer level
-    bytes32 public constant ADMIN_SIGNER_LEVEL =
-        keccak256(abi.encodePacked("ADMIN_SIGNER_LEVEL"));
-
-    //level => account => bool
-    mapping(bytes32 => mapping(address => bool)) public isAdmin;
     mapping(address => bool) public isSigner;
+    mapping(address => bool) public isAdmin;
+    //from tx id => owner => bool
+    mapping(uint256 => mapping(address => bool)) public txSigned;
+
+    //=======EVENTS=======//
 
     event WalletSetup(address indexed sender);
     event ReceivedDeposit(address indexed sender, uint256 value);
@@ -36,34 +38,36 @@ contract Multisigwallet {
         uint256 value,
         bytes data
     );
+    event SignedTransaction(address indexed sender, uint256 id);
     event Executed(address indexed sender, uint256 id);
     event SignaturesRequiredForTransaction(
         address indexed sender,
         uint8 signersRequired
     );
     event SetupSignersArray(address indexed sender, address[] signers);
+    event SetupAdminsArray(address indexed sender, address[] admins);
 
-    //=======MODIFIERS=======
+    //=======MODIFIERS=======//
 
     modifier onlySigners() {
-        require(isSigner[msg.sender], "Not an owner");
+        require(isSigner[msg.sender], "Not a signer");
         _;
     }
 
-    modifier onlyAdmin(bytes32 _signerLevel) {
-        require(isAdmin[_signerLevel][msg.sender], "Not an Admin");
+    modifier onlyAdmin() {
+        // require(isAdmin[_signerLevel][msg.sender], "Not an Admin");
+        require(isAdmin[msg.sender], "Not an admin");
         _;
     }
 
-    //=======CONSTRUCTOR=======
+    //=======CONSTRUCTOR=======//
 
-    constructor() {
-        grantAdminLevelOnSetup(ADMIN_SIGNER_LEVEL, msg.sender);
-
-        emit WalletSetup(msg.sender);
+    constructor(address _admin) {
+        isAdmin[_admin] = true;
+        emit WalletSetup(_admin);
     }
 
-    //=======FUNCTIONS=======
+    //=======FALLBACK FUNCTION=======//
 
     /**
      * @dev fallback function to receive any ether sent and emit an event indicating such.
@@ -72,33 +76,16 @@ contract Multisigwallet {
         emit ReceivedDeposit(msg.sender, msg.value);
     }
 
-    /**
-     * @dev Called on setup, granting an admin role.
-     */
-    function grantAdminLevelOnSetup(bytes32 _adminLevel, address _signerAccount)
-        internal
-    {
-        isAdmin[_adminLevel][_signerAccount] = true;
-    }
+    //=======TRANSACTION FUNCTIONs=======//
 
     /**
-     * @dev Grant an admin role after setup
-     */
-    function grantAdminLevel(bytes32 _adminLevel, address _signerAccount)
-        public
-        onlyAdmin(ADMIN_SIGNER_LEVEL)
-    {
-        isAdmin[_adminLevel][_signerAccount] = true;
-    }
-
-    /**
-     * @dev Submit a transaction to the multisig for consideration
+     * @dev Submit a transaction to the wallet for consideration
      */
     function submitTransaction(
         address _to,
         uint256 _valueDue,
         bytes memory _data
-    ) public onlySigners {
+    ) public onlySigners onlyAdmin {
         uint256 transactionId = transactionsArray.length;
 
         transactionsArray.push(
@@ -106,7 +93,7 @@ contract Multisigwallet {
                 recipient: _to,
                 valueDue: _valueDue,
                 data: _data,
-                numSigners: 0,
+                signaturesCollected: 0,
                 completed: false
             })
         );
@@ -115,29 +102,22 @@ contract Multisigwallet {
     }
 
     /**
-     * @dev Execute a transaction that has already been submitted to the multisig
+     * @dev Sign a transaction that has been submitted for consideration
      */
-    function executeTransaction(uint256 _transactionId) public onlySigners {
+    function signTransaction(uint256 _transactionId)
+        public
+        onlySigners
+        onlyAdmin
+    {
         Transaction storage transaction = transactionsArray[_transactionId];
-        transaction.numSigners += 1;
+        transaction.signaturesCollected += 1;
+        txSigned[_transactionId][msg.sender] = true;
 
-        require(
-            transaction.numSigners >= signaturesRequired,
-            "Not enough signatures for approval"
-        );
-
-        transaction.completed = true;
-
-        (bool success, ) = transaction.recipient.call{
-            value: transaction.valueDue
-        }("");
-        require(success, "Failed");
-
-        emit Executed(msg.sender, _transactionId);
+        emit SignedTransaction(msg.sender, _transactionId);
     }
 
     /**
-     * @dev Get data on a transaction
+     * @dev Get information about a transaction
      */
     function getTransaction(uint256 _transactionId)
         public
@@ -157,9 +137,32 @@ contract Multisigwallet {
             transaction.recipient,
             transaction.valueDue,
             transaction.data,
-            transaction.numSigners,
+            transaction.signaturesCollected,
             transaction.completed
         );
+    }
+
+    //=======ONLY ADMIN FUNCTIONS=======//
+
+    /**
+     * @dev Execute a transaction that has enough signatures.
+     */
+    function executeTransaction(uint256 _transactionId) public onlyAdmin {
+        Transaction storage transaction = transactionsArray[_transactionId];
+
+        require(
+            transaction.signaturesCollected >= signaturesRequired,
+            "Not enough signatures for approval"
+        );
+
+        transaction.completed = true;
+
+        (bool success, ) = transaction.recipient.call{
+            value: transaction.valueDue
+        }("");
+        require(success, "Failed");
+
+        emit Executed(msg.sender, _transactionId);
     }
 
     /**
@@ -167,7 +170,7 @@ contract Multisigwallet {
      */
     function signaturesRequiredForTx(uint8 _signaturesRequired)
         public
-        onlyAdmin(ADMIN_SIGNER_LEVEL)
+        onlyAdmin
     {
         require(
             _signaturesRequired > 0 && _signaturesRequired <= 256,
@@ -182,10 +185,7 @@ contract Multisigwallet {
     /**
      * @dev The admin can setup an array of multisig signers
      */
-    function setupSignersArray(address[] memory _signers)
-        public
-        onlyAdmin(ADMIN_SIGNER_LEVEL)
-    {
+    function setupSignersArray(address[] memory _signers) public onlyAdmin {
         require(
             _signers.length > 0 && _signers.length <= 256,
             "Setup: No. signers incorect"
@@ -210,10 +210,7 @@ contract Multisigwallet {
     /**
      * @dev The admin can add signers to the multisig
      */
-    function addSignerToArray(address _newSigner)
-        public
-        onlyAdmin(ADMIN_SIGNER_LEVEL)
-    {
+    function addSignerToArray(address _newSigner) public onlyAdmin {
         require(
             !isSigner[_newSigner] && _newSigner != address(0),
             "Cannot add signer"
@@ -226,10 +223,7 @@ contract Multisigwallet {
     /**
      * @dev The admin can remove signers from the array and, thus, the multisig
      */
-    function removeSigner(address _badSigner)
-        public
-        onlyAdmin(ADMIN_SIGNER_LEVEL)
-    {
+    function removeSigner(address _badSigner) public onlyAdmin {
         require(isSigner[_badSigner], "Not a signer");
 
         //cast address to uint so delete can be used
@@ -240,9 +234,69 @@ contract Multisigwallet {
     }
 
     /**
+     * @dev The admin can setup an array of multisig admins
+     */
+    function setupAdminArray(address[] memory _admins) public onlyAdmin {
+        require(
+            _admins.length > 0 && _admins.length <= 256,
+            "Setup: No. admins incorect"
+        );
+
+        for (uint8 i; i < _admins.length; i++) {
+            address newAdmin = _admins[i];
+
+            if (newAdmin == address(0)) {
+                revert("Setup: Don't use address(0)");
+            } else if (isAdmin[newAdmin]) {
+                revert("Setup: Already a signer");
+            } else {
+                admins.push(newAdmin);
+                isAdmin[newAdmin] = true;
+            }
+        }
+
+        emit SetupAdminsArray(msg.sender, _admins);
+    }
+
+    /**
+     * @dev The admin can add admins to the wallet
+     */
+    function addAdminToArray(address _newAdmin) public onlyAdmin {
+        require(
+            !isAdmin[_newAdmin] && _newAdmin != address(0),
+            "Cannot add admin"
+        );
+
+        admins.push(_newAdmin);
+        isAdmin[_newAdmin] = true;
+    }
+
+    /**
+     * @dev The admin can remove admins from the array
+     */
+    function removeAdmin(address _badAdmin) public onlyAdmin {
+        require(isAdmin[_badAdmin], "Not an admin");
+
+        //cast address to uint so delete can be used
+        uint256 badAdminId = uint256(uint160(_badAdmin));
+
+        //does not change arr length, resets arr value to default value
+        delete admins[badAdminId];
+    }
+
+    //=======VIEW FUNCTIONS=======//
+
+    /**
      * @dev Returns the array of multisig signers
      */
     function getSignersArray() public view returns (address[] memory) {
         return signers;
+    }
+
+    /**
+     * @dev Returns the array of multisig admins
+     */
+    function getAdminsArray() public view returns (address[] memory) {
+        return admins;
     }
 }
